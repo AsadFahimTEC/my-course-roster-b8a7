58249509474db39da2f2a1126214cd2073484a09
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-14 19:58:00 +0600

    header style started and finish

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+             <h3>Course Registration</h3>
+        </div>
+    );
+};
+
+export default Header;

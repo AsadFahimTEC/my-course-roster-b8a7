4a99bb14f3f55f244dd1fd10842a4d5f5149cf22
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-14 23:43:45 +0600

    card section dynamically load data show finished

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,33 +1,42 @@
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react';
 import './Card.css'
 
 
 const Card = () => {
-  const [allCourse, setAllCourse] = useState([])
+  const [allCourses, setAllCourses] = useState([])
 
   useEffect(() =>{
     fetch('./data.json')
     .then(res=>res.json())
-    .then(data=>setAllCourse(data))
+    .then(data=>setAllCourses(data))
 
   }, []);
 
-  console.log(allCourse)
+ 
 
   return (
    <div className="container">
-    <div className="card-container">
-      <div className="card">
-        <div className="card-img">
-          <img className='photo' src="https://i.ibb.co/ZJDV3vc/Rectangle-2-2.png" alt="" />
-        </div>
-        <h2>fdssd</h2>
-        <p><small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit, dolor?</small></p>
-        <div className="info">
-          <p>Price</p>
-          <p>Credit</p>
-        </div>
-        <button className='card-btn'>Select</button>
+    <div className="home-container">
+      <div className="card-container">
+     {
+      allCourses.map((course) => ( 
+      <div key={course.id} className="card">
+      <div className="card-img">
+        <img className='photo' src={course.image} alt="" />
+      </div>
+      <h2 className='title'>{course.name}</h2>
+      <p className='details'><small>{course.details}</small></p>
+      <div className="info">
+        <p>$ Price: {course.price}</p>
+        <p>Credit: {course.credit}</p>
+      </div>
+      <button className='card-btn'>Select</button>
+    </div>
+))}
+      </div>
+      <div className="cart">
+        <h1>this is cart</h1>
       </div>
     </div>
    </div>

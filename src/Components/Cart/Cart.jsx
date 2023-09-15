508be51870f9e1bd911d67b9751bae2a6a4539d2
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-09-15 21:27:35 +0600

    cart section margin solve and responsive finish

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import "./Cart.css";
 
 const Cart = ({ selectedAllCourses, totalCredit, remainingCredit }) => {
   return (
-    <div className="cart">
+    <div className="carts">
       <h3>Credit Hour Remaining {remainingCredit} hr </h3>
       <hr />
       <h4>Course Name</h4>
